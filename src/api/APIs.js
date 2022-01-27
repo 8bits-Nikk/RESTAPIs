566729937f94e2686aaebac84ee3e9cc3c4afac8
Nikhil Makwana <64721742+8bits-Nikk@users.noreply.github.com>
2022-01-27 13:37:30 +0530

    Update APIs.js

--- src/api/APIs.js
+++ src/api/APIs.js
@@ -5,6 +5,6 @@ export const api = create({
     headers: {
         'Content-type': 'application/json',
         'Accept' : 'application/json',
-        'Authorization' : 'Bearer 37df5b7a26d9d2e0fc187c8311004c115e880f61ef6fd4f391bc532fc0f8de97'
+        'Authorization' : ''
     }
 })
